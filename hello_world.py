1c1644b5ff4ce5deadad96f1d05d0c15db6b62b5
Matt Wiens <mwiens91@gmail.com>
2018-08-08 11:29:26 -0700

    Line break the hello world messages

--- hello_world.py
+++ hello_world.py
@@ -10,10 +10,10 @@ args = json.loads(sys.argv[1])
 
 with open('/logs/mylogfile.txt', 'w') as f:
     # Write hello
-    f.write("Hello world %s" % args['name'])
+    f.write("Hello world %s\n" % args['name'])
     f.write("Hello shell %s" % os.environ['SHELL'])
 
 with open('/results/myresultsfile.txt', 'w') as f:
     # Write hello again
-    f.write("Hello world %s" % args['name'])
+    f.write("Hello world %s\n" % args['name'])
     f.write("Hello shell %s" % os.environ['SHELL'])
