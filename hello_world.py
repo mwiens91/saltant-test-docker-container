69af72459d64aeaa484a2d0827f008ddfcab7461
Matt Wiens <mwiens91@gmail.com>
2018-08-08 12:19:02 -0700

    Add Shahlab specific things

--- hello_world.py
+++ hello_world.py
@@ -8,15 +8,28 @@ import sys
 # argument
 args = json.loads(sys.argv[1])
 
+worker_logs_path = os.path.join(
+    os.environ['WORKER_LOGS_DIRECTORY'],
+    os.environ['JOB_UUID'],)
+worker_results_path = os.path.join(
+    os.environ['WORKER_RESULTS_DIRECTORY'],
+    os.environ['JOB_UUID'],)
+
+os.makedirs(worker_logs_path, exist_ok=True)
+os.makedirs(worker_results_path, exist_ok=True)
+
+logfilepath = os.path.join(worker_logs_path, 'mylogfile.txt')
+resultsfilepath = os.path.join(worker_results_path, 'myresultsfile.txt')
+
 def hello(name, file_):
     print("Hello world %s" % name, file=file_)
     print("Hello job %s" % os.environ['JOB_UUID'], file=file_)
     print("Hello shell %s" % os.environ['SHELL'], file=file_)
 
-with open('/logs/mylogfile.txt', 'w') as f:
+with open(logfilepath, 'w') as f:
     # Write hello
     hello(args['name'], f)
 
-with open('/results/myresultsfile.txt', 'w') as f:
+with open(resultsfilepath, 'w') as f:
     # Write hello again
     hello(args['name'], f)
